29939b4eaf44231285bc93e754214d6b9119a7e8
jcarrharris <joncarrharris@gmail.com>
2013-09-08 18:52:00 -0400

    creating login in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rainforest::Application.routes.draw do
   
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   get "users/new"
 
   get "users/create"
 
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
   root :to => "products#index"
 
   # The priority is based upon order of creation:
