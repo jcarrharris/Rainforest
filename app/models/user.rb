0f837efa57c6cde148bc24b4578f408b9cb0af42
jcarrharris <joncarrharris@gmail.com>
2013-09-08 23:33:42 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :name
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates_presence_of :name
 end
